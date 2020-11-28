402c07407b0773eb7c5fbd95def7cec48bc790a0
Gakinchi <49122586+Gakinchi@users.noreply.github.com>
2020-11-28 12:37:29 +0100

    changed app text and switched to feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Live change!
+          Changing the web application in small steps at a time.
         </p>
         <a
           className="App-link"
