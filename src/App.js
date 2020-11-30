79d34fddfe568ea2c0fda6566e4f51ad00834f4e
Gakinchi <49122586+Gakinchi@users.noreply.github.com>
2020-11-30 16:19:09 +0100

    changed opening text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Changing the web application in small steps at a time.
+          WELCOME TO REACT!
         </p>
         <a
           className="App-link"
